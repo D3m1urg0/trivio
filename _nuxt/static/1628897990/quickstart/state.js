62a2baa7089e069b9fa211d2f9599b287d4b37e0
D3m1urg0 <p.caruso86@gmail.com>
2021-08-13 23:58:25 +0000

    Deploying to gh-pages from  @ fe48204714339dd58e73b34c8ab899855ce3edb0 🚀

--- _nuxt/static/1628897990/quickstart/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1628897990",layout:"void",error:a,serverRendered:true,routePath:"\u002Fquickstart",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"948c6ae7"}}}}(null));
