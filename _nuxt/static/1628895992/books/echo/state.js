0727ad1e6e8afce2f78874419f82861f1c359dd6
D3m1urg0 <p.caruso86@gmail.com>
2021-08-13 23:17:54 +0000

    Deploying to gh-pages from  @ 5646badda59680bdb05860de7fc4bce15a8faad1 🚀

--- _nuxt/static/1628895992/books/echo/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1628895992",layout:"default",error:a,serverRendered:true,routePath:"\u002Fbooks\u002Fecho",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c3b6e973"}}}}(null));
