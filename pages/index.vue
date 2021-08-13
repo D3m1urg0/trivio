117244f59b6db983065075f75d6564fc2f771a39
Pietro Caruso <pietro.caruso@agictech.com>
2021-08-13 20:14:01 +0200

    autunno

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,11 @@
   <div>
     <main class="bg-main">
       <div
-        class="container relative flex justify-center h-full px-4 py-8 mx-auto border-t border-brand-light"
+        class="border-brand-light container relative flex justify-center h-full px-4 py-8 mx-auto border-t"
       >
         <raindrops class="pointer-events-none" />
         <div
-          class="z-10 flex flex-col items-center flex-1 max-w-2xl md:flex-row"
+          class="md:flex-row z-10 flex flex-col items-center flex-1 max-w-2xl"
         >
           <div class="flex justify-center flex-1 p-4">
             <div class="flex justify-center w-64 h-64">
@@ -23,7 +23,7 @@
       class="container flex flex-wrap justify-around px-4 py-12 mx-auto text-center"
     >
       <div class="flex-1 p-8">
-        <h3 class="mb-2 text-lg font-bold text-brand-light">LIBRIGAME</h3>
+        <h3 class="text-brand-light mb-2 text-lg font-bold">LIBRIGAME</h3>
         <p>
           Collana Trivio è dedicata all'esplorazione del Librogame attraverso
           soluzioni narrative sperimentali e autori molto diversi tra loro.
@@ -31,22 +31,22 @@
       </div>
 
       <div class="flex-1 p-8">
-        <h3 class="mb-2 text-lg font-bold text-brand-light">TRE STRADE</h3>
+        <h3 class="text-brand-light mb-2 text-lg font-bold">TRE STRADE</h3>
         <p>
           Il numero tre è fondamentale nel nostro progetto. Tre storie, tre
           protagonisti, tre modi diversi di vivere l'avventura.
         </p>
       </div>
       <div class="flex-1 p-8">
-        <h3 class="mb-2 text-lg font-bold text-brand-light">CROWDFUNDING</h3>
+        <h3 class="text-brand-light mb-2 text-lg font-bold">CROWDFUNDING</h3>
         <p>
-          Primavera 2021: è la nostra data. Il progetto partirà da un
+          Autunno 2021: è la nostra data. Il progetto partirà da un
           crowdfunding, e questo è il posto migliore per rimanere aggiornati.
         </p>
       </div>
     </div>
     <div class="bg-brand-dark">
-      <book-list class="flex flex-col p-10 lg:flex-row" :books="books" />
+      <book-list class="lg:flex-row flex flex-col p-10" :books="books" />
     </div>
   </div>
 </template>
